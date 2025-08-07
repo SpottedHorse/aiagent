7077efd537a2d8df4596a5b85945792fca5dd320
SpottedHorse <alexbelanger1996@hotmail.com>
2025-08-07 15:05:22 -0400

    added functions and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,10 +1,24 @@
-import sys
 from os import path, listdir
 
 def get_files_info(working_directory, directory="."):
-    path.join(working_directory, directory)
-    
+    abs_working_dir = path.abspath(working_directory)
+    target_dir = path.abspath(path.join(working_directory, directory))
     
     # If the absolute path to the directory is outside the working_directory, return a string error message:
-    if directory not in working_directory:
-        pass
+    # if the abspath of 'directory' ends before the working directory return an error
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    try:
+        files_info = []
+        for filename in listdir(target_dir):
+            filepath = path.join(target_dir, filename)
+            file_size = 0
+            is_dir = path.isdir(filepath)
+            file_size = path.getsize(filepath)
+            files_info.append(f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files: {e}"
