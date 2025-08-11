6cc629a852a20da597cc9ac1a0add998feddc240
SpottedHorse <alexbelanger1996@hotmail.com>
2025-08-11 09:30:12 -0400

    add write file function

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -3,15 +3,19 @@ from config import *
 
 
 def get_file_content(working_directory, file_path):
-  abs_working_dir = path.abspath(working_directory)
-  target_path = path.abspath(path.join(working_directory, file_path))
-  
-  if not target_path.startswith(abs_working_dir):
-      return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-  
-  if not path.isfile(file_path):
-     return f'Error: File not found or is not a regular file: "{file_path}"'
-  
-  try:  
-    with open(file_path, "r") as f:
-      file_content_string = f.read(MAX_CHARS)
+    abs_working_dir = path.abspath(working_directory)
+    abs_file_path = path.abspath(path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
+# from functions.get_file_content import get_file_content
+from functions.write_file_content import write_file
 
 
 def test():
@@ -12,23 +14,40 @@ def test():
     #result = get_file_content("calculator", "/bin/cat")
     #print(result)
     
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
+    # result = get_files_info("calculator", ".")
+    # print("Result for current directory:")
+    # print(result)
+    # print("")
 
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    # result = get_files_info("calculator", "pkg")
+    # print("Result for 'pkg' directory:")
+    # print(result)
+
+    # result = get_files_info("calculator", "/bin")
+    # print("Result for '/bin' directory:")
+    # print(result)
+
+    # result = get_files_info("calculator", "../")
+    # print("Result for '../' directory:")
+    # print(result)
+
+    # result = get_file_content("calculator", "main.py")
+    # print(result)
+
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print(result)
+
+    # result = get_file_content("calculator", "/bin/cat")
+    # print(result)
+    
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
-
 
 if __name__ == "__main__":
     test()
