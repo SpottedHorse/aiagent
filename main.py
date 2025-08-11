a88a16bc609bced68bd389ad9b4275b29c8df3e4
SpottedHorse <alexbelanger1996@hotmail.com>
2025-08-11 10:20:20 -0400

    add system_prompt parameter

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from google.genai import types
 def main():
     load_dotenv()
     
-    v = "--verbose" in sys.argv
-    args = []
-    for arg in sys.argv[1:]:
+    system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+    v = "--verbose" in sys.argv  #if --verbose flag is included in cli args v == True
+    args = []  #create an empty list to store the args
+
+    for arg in sys.argv[1:]:  #add args to args list excpluding first entry which is the filename
         if not arg.startswith("--"):
             args.append(arg)
 
-    if not args:
+    if not args:  # if no args print usage instructions
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
@@ -24,22 +26,24 @@ def main():
 
     user_prompt = " ".join(args)
 
-    if v:
+    if v:  #if --verbose arg was called also print the user prompt
         print(f"User prompt: {user_prompt}\n")
     
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    p_config = types.GenerateContentConfig(system_instruction=system_prompt)
+    
+    generate_content(client, messages, p_config, v)
 
-    generate_content(client, user_prompt, v)
-
-def generate_content(client, messages, v):
+def generate_content(client, messages, p_config, v):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config=p_config
     )
     print("Response:")
     print(response.text)
 
-    if v:
+    if v:  #if --verbose arg was called also print report on token usage
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
